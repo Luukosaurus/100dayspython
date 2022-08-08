257fe544191ab1660f4b2ddc92e7057ec52d431a
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-08-08 19:45:29 +0200

    Day 1 first commit input vars and print

---
+++ day 1.py
@@ -0,0 +1,7 @@
+print("Hello world")
+naam = input("Wie ben jij\n")
+print("Yo " + naam)
+print("je naam is "+ str(len(naam)) +" tekens lang")
+woonstraat = input("welke straat woon je\n")
+huisnr = input("Wat is je huis nummer\n")
+print("Je woont op " + woonstraat +" "+ huisnr)
