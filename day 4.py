45ebac333acd16126a6c4a9b242ee0e50d935ff0
Luuk Schukkink <luuk.schukkink@gmail.com>
2022-08-11 16:05:46 +0200

    na les fixes list and beter if statements

--- day 4.py
+++ day 4.py
@@ -1,44 +1,47 @@
 import random
-randomnr = random.randint(1,3)
-userinput = input("Rock(1) Paper(2) Scissors(3)\n")
-if userinput == "1" or userinput == "rock" or userinput == "Rock" or userinput == "steen" or userinput == "Steen":
-    print("you chose Rock")
-    if randomnr == 1:
-        print("The computer chose Rock")
-        print("You had a draw")
-    elif randomnr == 2:
-        print("The computer chose Paper")
-        print("You lost")
-    elif randomnr == 3:
-        print("The computer chose Scissors")
-        print("You won")
-    else:
-        print("Error: Random input not valid")
-elif userinput == "2" or userinput == "paper" or userinput == "Paper" or userinput == "papier" or userinput == "Papier":
-    print("you chose Paper")
-    if randomnr == 1:
-        print("The computer chose Rock")
-        print("You won")
-    elif randomnr == 2:
-        print("The computer chose Paper")
-        print("You had a draw")
-    elif randomnr == 3:
-        print("The computer chose Scissors")
-        print("You lost")
-    else:
-        print("Error: Random input not valid")
-elif userinput == "3" or userinput == "scissors" or userinput == "=Scissors" or userinput == "schaar" or userinput == "Schaar":
-    print("you chose Scissors")
-    if randomnr == 1:
-        print("The computer chose Rock")
-        print("You lost")
-    elif randomnr == 2:
-        print("The computer chose Paper")
-        print("You won")
-    elif randomnr == 3:
-        print("The computer chose Scissors")
-        print("You had a draw")
-    else:
-        print("Error: Random input not valid")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+randomnr = random.randint(0,2)
+asciiimage = [rock,paper,scissors]
+namenrs = ["Rock","Paper","Scissors"]
+userinput = int(input("Rock(1) Paper(2) Scissors(3)\n")) -1
+if(userinput >2 or userinput < 0):
+    print("Wrong input try again")
+    exit()
+print(f"You chose {namenrs[userinput]}")
+print(asciiimage[userinput])
+print(f"The computer chose {namenrs[randomnr]}")
+print(asciiimage[randomnr])
+if(userinput == randomnr):
+    print("It's a draw")
+elif(userinput - randomnr == -1 or userinput - randomnr == 2):
+    print("You lost")
+elif(userinput - randomnr == 1 or userinput - randomnr == -2):
+    print("You won")
 else:
-    print("invalid input try again")
+    print(f"Something went wrong you chose {userinput} the computer chose {randomnr}")
